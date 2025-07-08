6d82a11b62213805901afc431e4f04fb095f939f
Milad Shalikar <miladsh199@gmail.com>
2025-07-09 01:29:49 +0330

    hotfix - category endpoint

--- delivery/http_server/server.go
+++ delivery/http_server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/miladshalikar/cafe/config"
+	categoryhandler "github.com/miladshalikar/cafe/delivery/http_server/category"
 	userauthhandler "github.com/miladshalikar/cafe/delivery/http_server/user/auth"
 	userprofilehandler "github.com/miladshalikar/cafe/delivery/http_server/user/profile"
 )
@@ -13,22 +14,26 @@ type Server struct {
 	Router             *echo.Echo
 	userAuthHandler    userauthhandler.Handler
 	userProfileHandler userprofilehandler.Handler
+	categoryHandler    categoryhandler.Handler
 }
 
 func New(config config.Config,
 	userAuthHandler userauthhandler.Handler,
-	userProfileHandler userprofilehandler.Handler) Server {
+	userProfileHandler userprofilehandler.Handler,
+	categoryHandler categoryhandler.Handler) Server {
 	return Server{
 		config:             config,
 		Router:             echo.New(),
 		userAuthHandler:    userAuthHandler,
 		userProfileHandler: userProfileHandler,
+		categoryHandler:    categoryHandler,
 	}
 }
 
 func (s Server) Serve() {
 	s.userAuthHandler.SetRoutes(s.Router)
 	s.userProfileHandler.SetRoutes(s.Router)
+	s.categoryHandler.SetRoutes(s.Router)
 
 	address := fmt.Sprintf(":%d", s.config.Server.Port)
 	fmt.Printf("start echo server on %s\n", address)
--- main.go
+++ main.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"github.com/miladshalikar/cafe/config"
 	httpserver "github.com/miladshalikar/cafe/delivery/http_server"
+	categoryhandler "github.com/miladshalikar/cafe/delivery/http_server/category"
 	userauthhandler "github.com/miladshalikar/cafe/delivery/http_server/user/auth"
 	userprofilehandler "github.com/miladshalikar/cafe/delivery/http_server/user/profile"
 	"github.com/miladshalikar/cafe/repository/migrator"
 	"github.com/miladshalikar/cafe/repository/postgreSQL"
+	categorypostgresql "github.com/miladshalikar/cafe/repository/postgreSQL/category"
 	userpostgresql "github.com/miladshalikar/cafe/repository/postgreSQL/user"
+	categoryservice "github.com/miladshalikar/cafe/service/categoty"
 	userauthservice "github.com/miladshalikar/cafe/service/user/authservice"
 	userprofileservice "github.com/miladshalikar/cafe/service/user/profile"
 	usertokenauthservice "github.com/miladshalikar/cafe/service/user/token"
+	categoryvalidator "github.com/miladshalikar/cafe/validator/category"
 	userauthvalidator "github.com/miladshalikar/cafe/validator/user/auth"
 )
 
 func main() {
 
-	fmt.Println("hello worldd")
+	fmt.Println("hello world")
 	cfg := config.C()
 	m := migrator.New(cfg.Postgres)
 	m.Up()
@@ -29,8 +33,15 @@ func main() {
 	val := userauthvalidator.New(repo)
 	ser := userauthservice.New(repo, tok)
 	hand := userauthhandler.New(ser, val)
+
 	aaa := userprofileservice.New(repo)
 	handy := userprofilehandler.New(aaa, tok, cfg.Token)
-	echoServer := httpserver.New(cfg, hand, handy)
+
+	rrr := categorypostgresql.New(db)
+	nn := categoryvalidator.New(rrr)
+	sss := categoryservice.New(rrr)
+	hhh := categoryhandler.New(sss, nn, tok, cfg.Token)
+
+	echoServer := httpserver.New(cfg, hand, handy, hhh)
 	echoServer.Serve()
 }
