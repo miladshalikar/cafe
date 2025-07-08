f14a2352ea5e2dd1733173500e69b41c48e402d7
Milad Shalikar <miladsh199@gmail.com>
2025-07-08 19:04:28 +0330

    add category service

---
+++ service/categoty/update.go
@@ -0,0 +1,25 @@
+package categotyservice
+
+import (
+	"context"
+	"github.com/miladshalikar/cafe/entity"
+	categoryparam "github.com/miladshalikar/cafe/param/category"
+)
+
+func (s Service) UpdateCategory(ctx context.Context, req categoryparam.UpdateCategoryRequest) (categoryparam.UpdateCategoryResponse, error) {
+	category := entity.Category{
+		Id:    req.ID,
+		Title: req.Title,
+		Logo:  req.Logo,
+	}
+
+	if cErr := s.repo.UpdateCategory(ctx, category); cErr != nil {
+		return categoryparam.UpdateCategoryResponse{}, cErr
+	}
+
+	return categoryparam.UpdateCategoryResponse{
+		ID:    category.Id,
+		Title: category.Title,
+		Logo:  category.Logo,
+	}, nil
+}
