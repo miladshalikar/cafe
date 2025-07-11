41c0a7d1b52bbf83af2f66d905b4193778398460
Milad Shalikar <miladsh199@gmail.com>
2025-07-11 23:39:21 +0330

    refactore acl middleware service

--- repository/postgreSQL/acl/acl.go
+++ repository/postgreSQL/acl/acl.go
@@ -1,22 +1,89 @@
 package aclpostgresql
 
-func (d *DB) HasPermission(userID uint, permissionTitle string) (int, error) {
+func (d *DB) GetPermissionIDsByUserID(userID uint) ([]uint, error) {
 
-	query := `SELECT COUNT(1)
-		FROM permissions p
-		WHERE p.title = $1 AND p.id IN (
-			SELECT pu.permission_id FROM permission_user pu WHERE pu.user_id = $2
-			UNION
-			SELECT pr.permission_id
-			FROM permission_role pr
-			JOIN role_user ru ON ru.role_id = pr.role_id
-			WHERE ru.user_id = $2
-		)`
+	query := `
+		SELECT permission_id
+		FROM permission_user
+		WHERE user_id = $1 AND deleted_at IS NULL
+		`
 
-	var count int
-	err := d.conn.QueryRow(query, permissionTitle, userID).Scan(&count)
+	rows, rErr := d.conn.Query(query, userID)
+	if rErr != nil {
+		return nil, rErr
+	}
+	defer rows.Close()
+
+	var permissionIDs []uint
+	for rows.Next() {
+		var id uint
+		if err := rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		permissionIDs = append(permissionIDs, id)
+	}
+	return permissionIDs, nil
+}
+
+func (d *DB) GetRoleIDsByUserID(userID uint) ([]uint, error) {
+	query := `
+		SELECT role_id
+		FROM role_user
+		WHERE user_id = $1 AND deleted_at IS NULL
+	`
+
+	rows, rErr := d.conn.Query(query, userID)
+	if rErr != nil {
+		return nil, rErr
+	}
+	defer rows.Close()
+
+	var roleIDs []uint
+	for rows.Next() {
+		var id uint
+		if err := rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		roleIDs = append(roleIDs, id)
+	}
+	return roleIDs, nil
+}
+
+func (d *DB) GetPermissionIDsByRoleID(roleID uint) ([]uint, error) {
+	query := `
+		SELECT permission_id
+		FROM permission_role
+		WHERE role_id = $1 AND deleted_at IS NULL
+	`
+
+	rows, rErr := d.conn.Query(query, roleID)
+	if rErr != nil {
+		return nil, rErr
+	}
+	defer rows.Close()
+
+	var permissionIDs []uint
+	for rows.Next() {
+		var id uint
+		if err := rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		permissionIDs = append(permissionIDs, id)
+	}
+	return permissionIDs, nil
+}
+
+func (d *DB) GetPermissionIDByTitle(title string) (uint, error) {
+	query := `
+		SELECT id
+		FROM permissions
+		WHERE title = $1 AND deleted_at IS NULL
+	`
+
+	var id uint
+	err := d.conn.QueryRow(query, title).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
-	return count, nil
+	return id, nil
 }
--- service/acl/acl.go
+++ service/acl/acl.go
@@ -1,9 +1,62 @@
 package aclservice
 
+import "fmt"
+
 func (s Service) HasPermission(userID uint, permissionTitle string) (bool, error) {
-	count, err := s.repo.HasPermission(userID, permissionTitle)
+
+	permissionIDsList, err := s.repo.GetPermissionIDsByUserID(userID)
 	if err != nil {
 		return false, err
 	}
-	return count > 0, nil
+
+	fmt.Println("1", permissionIDsList)
+
+	roleIDsList, err := s.repo.GetRoleIDsByUserID(userID)
+	if err != nil {
+		return false, err
+	}
+
+	fmt.Println("2", roleIDsList)
+
+	var allPermissionIDsList []uint
+
+	for _, roleID := range roleIDsList {
+		permissionIDs, pErr := s.repo.GetPermissionIDsByRoleID(roleID)
+		if pErr != nil {
+			return false, pErr
+		}
+
+		fmt.Println("33", permissionIDs)
+
+		allPermissionIDsList = append(allPermissionIDsList, permissionIDs...)
+	}
+
+	allPermissionIDsList = append(allPermissionIDsList, permissionIDsList...)
+
+	fmt.Println("4", allPermissionIDsList)
+
+	PermissionID, iDrr := s.repo.GetPermissionIDByTitle(permissionTitle)
+	if iDrr != nil {
+		return false, iDrr
+	}
+
+	fmt.Println("5", PermissionID)
+
+	if hasID(allPermissionIDsList, PermissionID) {
+		return true, nil
+	} else {
+		return false, nil
+	}
+
+}
+
+func hasID(list []uint, target uint) bool {
+
+	for _, id := range list {
+		if id == target {
+			return true
+		}
+	}
+	return false
+
 }
