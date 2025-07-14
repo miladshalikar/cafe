1d8301349442889945b32711f3b2c63f380886ac
Milad Shalikar <miladsh199@gmail.com>
2025-07-14 17:49:33 +0330

    hotfix - category and item entity

--- entity/Item.go
+++ entity/Item.go
@@ -8,7 +8,7 @@ type Item struct {
 	Description string     `json:"description"`
 	Price       float64    `json:"price"`
 	CategoryId  uint       `json:"category_id"`
-	Logo        string     `json:"logo"`
+	MediaID     uint       `json:"media_id"`
 	CreatedAt   time.Time  `json:"created_at"`
 	UpdatedAt   time.Time  `json:"updated_at"`
 	DeletedAt   *time.Time `json:"deleted_at"`
