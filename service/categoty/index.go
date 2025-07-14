19893ef135064b0306d175c89ae434c4112e7e9f
Milad Shalikar <miladsh199@gmail.com>
2025-07-15 00:10:44 +0330

    add index service

---
+++ service/categoty/index.go
@@ -0,0 +1,41 @@
+package categoryservice
+
+import (
+	"context"
+	categoryparam "github.com/miladshalikar/cafe/param/category"
+	"github.com/miladshalikar/cafe/param/common"
+)
+
+func (s Service) GetCategories(ctx context.Context, req categoryparam.GetCategoryRequest) (categoryparam.GetCategoryResponse, error) {
+
+	total, tErr := s.repo.GetTotalCountArea(ctx)
+	if tErr != nil {
+		return categoryparam.GetCategoryResponse{}, tErr
+	}
+
+	categories, cErr := s.repo.GetAreasWithPagination(ctx, req.Pagination.GetPageSize(), req.Pagination.GetOffset())
+	if cErr != nil {
+		return categoryparam.GetCategoryResponse{}, cErr
+	}
+
+	var categoriesInfo []categoryparam.CategoryInfo
+	for _, category := range categories {
+
+		categoriesInfo = append(categoriesInfo, categoryparam.CategoryInfo{
+			ID:      category.ID,
+			Title:   category.Title,
+			MediaID: category.MediaID,
+		})
+
+	}
+
+	return categoryparam.GetCategoryResponse{
+		Pagination: common.PaginationResponse{
+			PageSize:   req.Pagination.GetPageSize(),
+			PageNumber: req.Pagination.GetPageNumber(),
+			Total:      total,
+		},
+		Categories: categoriesInfo,
+	}, nil
+
+}
