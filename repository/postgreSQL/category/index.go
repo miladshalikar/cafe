53acc9f0b6f01e76c837945a8dd0940beccaf5c9
Milad Shalikar <miladsh199@gmail.com>
2025-07-15 18:18:10 +0330

    update index and show methods for categoryRepo

--- repository/postgreSQL/category/index.go
+++ repository/postgreSQL/category/index.go
@@ -2,13 +2,45 @@ package categorypostgresql
 
 import (
 	"context"
+	"fmt"
 	"github.com/miladshalikar/cafe/entity"
 )
 
-func (d *DB) GetTotalCountArea(ctx context.Context) (uint, error) {
-	panic("aa")
+func (d *DB) GetTotalCountCategory(ctx context.Context) (uint, error) {
+
+	query := `SELECT * FROM categories`
+
+	var count uint
+
+	if err := d.conn.QueryRowContext(ctx, query).Scan(&count); err != nil {
+		return 0, fmt.Errorf("something went wrong: %w", err)
+	}
+	return count, nil
+
 }
 
-func (d *DB) GetAreasWithPagination(ctx context.Context, pageSize, offset uint) ([]entity.Category, error) {
-	panic("aaa")
+func (d *DB) GetCategoriesWithPagination(ctx context.Context, pageSize, offset uint) ([]entity.Category, error) {
+
+	query := `SELECT * FROM categories LIMIT $1 OFFSET $2`
+
+	rows, err := d.conn.QueryContext(ctx, query, pageSize, offset)
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %w", err)
+	}
+	defer rows.Close()
+
+	var categories []entity.Category
+
+	for rows.Next() {
+		category, cErr := scanCategory(rows)
+		if cErr != nil {
+			return nil, fmt.Errorf("scanning category failed: %w", err)
+		}
+		categories = append(categories, category)
+	}
+	if rErr := rows.Err(); rErr != nil {
+		return nil, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return categories, nil
 }
--- repository/postgreSQL/category/show.go
+++ repository/postgreSQL/category/show.go
@@ -25,3 +25,20 @@ func (d *DB) GetCategoryById(ctx context.Context, id uint) (entity.Category, err
 	return category, nil
 
 }
+
+func (d *DB) CheckCategoryIsExistByID(ctx context.Context, id uint) (bool, error) {
+
+	query := `SELECT * FROM categories WHERE id = $1`
+
+	row := d.conn.QueryRowContext(ctx, query, id)
+
+	_, err := scanCategory(row)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to scan category: %w", err)
+	}
+
+	return true, nil
+}
