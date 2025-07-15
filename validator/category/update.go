ae38aede8698750e2a89efbe41c2f04a4153984d
Milad Shalikar <miladsh199@gmail.com>
2025-07-15 15:59:57 +0330

    add update method for categoryValidator

---
+++ validator/category/update.go
@@ -0,0 +1,28 @@
+package categoryvalidator
+
+import (
+	"context"
+	"errors"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	categoryparam "github.com/miladshalikar/cafe/param/category"
+)
+
+func (v Validator) ValidateUpdateCategory(ctx context.Context, req categoryparam.UpdateCategoryRequest) (map[string]string, error) {
+
+	if err := validation.ValidateStructWithContext(ctx, &req,
+		validation.Field(&req.Title, validation.Required, validation.Length(3, 190)),
+	); err != nil {
+		fieldErrors := make(map[string]string)
+		vErr := validation.Errors{}
+		if errors.As(err, &vErr) {
+			for key, value := range vErr {
+				if value != nil {
+					fieldErrors[key] = value.Error()
+				}
+			}
+		}
+		return fieldErrors, err
+	}
+
+	return nil, nil
+}
