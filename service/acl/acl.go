a1ad1aec2087eedeebc5fd16548c096c6e4cdf83
Milad Shalikar <miladsh199@gmail.com>
2025-07-16 20:23:42 +0330

    hotfix - acl service

--- service/acl/acl.go
+++ service/acl/acl.go
@@ -1,7 +1,5 @@
 package aclservice
 
-import "fmt"
-
 func (s Service) HasPermission(userID uint, permissionTitle string) (bool, error) {
 
 	permissionIDsList, err := s.repo.GetPermissionIDsByUserID(userID)
@@ -9,14 +7,10 @@ func (s Service) HasPermission(userID uint, permissionTitle string) (bool, error
 		return false, err
 	}
 
-	fmt.Println("1", permissionIDsList)
-
 	roleIDsList, err := s.repo.GetRoleIDsByUserID(userID)
 	if err != nil {
 		return false, err
 	}
-
-	fmt.Println("2", roleIDsList)
 
 	var allPermissionIDsList []uint
 
@@ -26,21 +20,15 @@ func (s Service) HasPermission(userID uint, permissionTitle string) (bool, error
 			return false, pErr
 		}
 
-		fmt.Println("33", permissionIDs)
-
 		allPermissionIDsList = append(allPermissionIDsList, permissionIDs...)
 	}
 
 	allPermissionIDsList = append(allPermissionIDsList, permissionIDsList...)
 
-	fmt.Println("4", allPermissionIDsList)
-
 	PermissionID, iDrr := s.repo.GetPermissionIDByTitle(permissionTitle)
 	if iDrr != nil {
 		return false, iDrr
 	}
-
-	fmt.Println("5", PermissionID)
 
 	if hasID(allPermissionIDsList, PermissionID) {
 		return true, nil
