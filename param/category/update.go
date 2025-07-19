642afb73ac2a1ded4538fd30eab28ef68c021a11
Milad Shalikar <miladsh199@gmail.com>
2025-07-19 17:11:44 +0330

    hotfix - category param

--- param/category/update.go
+++ param/category/update.go
@@ -1,8 +1,9 @@
 package categoryparam
 
 type UpdateCategoryRequest struct {
-	ID    uint   `json:"id"`
-	Title string `json:"title"`
+	ID      uint   `json:"id"`
+	Title   string `json:"title"`
+	MediaID uint   `json:"media_id"`
 }
 
 type UpdateCategoryResponse struct {
