2d2eee4503419eba3f0d453a43272bacc35c41a9
Milad Shalikar <miladsh199@gmail.com>
2025-07-19 18:34:38 +0330

    hotfix - category service

--- service/categoty/update.go
+++ service/categoty/update.go
@@ -4,22 +4,43 @@ import (
 	"context"
 	"github.com/miladshalikar/cafe/entity"
 	categoryparam "github.com/miladshalikar/cafe/param/category"
+	mediaparam "github.com/miladshalikar/cafe/param/media"
 )
 
 func (s Service) UpdateCategory(ctx context.Context, req categoryparam.UpdateCategoryRequest) (categoryparam.UpdateCategoryResponse, error) {
-	category := entity.Category{
-		ID:    req.ID,
-		Title: req.Title,
+
+	currentCategory, cErr := s.repo.GetCategoryByID(ctx, req.ID)
+	if cErr != nil {
+		return categoryparam.UpdateCategoryResponse{}, cErr
 	}
 
-	if cErr := s.repo.UpdateCategory(ctx, category); cErr != nil {
-		return categoryparam.UpdateCategoryResponse{}, cErr
+	if currentCategory.MediaID != req.MediaID {
+
+		if _, sErr := s.Client.DeleteMedia(ctx, mediaparam.DeleteMediaRequest{ID: currentCategory.MediaID}); sErr != nil {
+			return categoryparam.UpdateCategoryResponse{}, sErr
+		}
+	}
+
+	category := entity.Category{
+		ID:      req.ID,
+		Title:   req.Title,
+		MediaID: req.MediaID,
+	}
+
+	if rErr := s.repo.UpdateCategory(ctx, category); rErr != nil {
+		return categoryparam.UpdateCategoryResponse{}, rErr
+	}
+	mediaURL, uErr := s.Client.GetURLMedia(ctx, mediaparam.GetURLRequest{ID: category.MediaID})
+	if uErr != nil {
+		return categoryparam.UpdateCategoryResponse{}, uErr
 	}
 
 	return categoryparam.UpdateCategoryResponse{
 		Category: categoryparam.CategoryInfo{
-			ID:    category.ID,
-			Title: category.Title,
+			ID:      category.ID,
+			Title:   category.Title,
+			MediaID: category.MediaID,
+			URL:     mediaURL.URL,
 		},
 	}, nil
 }
