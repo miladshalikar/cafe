9a328ce54a2e1f30e00ca06bb7bcaae78da21ba9
Milad Shalikar <miladsh199@gmail.com>
2025-07-22 00:54:31 +0330

    add redis to category service

--- service/categoty/index.go
+++ service/categoty/index.go
@@ -3,7 +3,7 @@ package categoryservice
 import (
 	"context"
 	categoryparam "github.com/miladshalikar/cafe/param/category"
-	"github.com/miladshalikar/cafe/param/common"
+	commonparam "github.com/miladshalikar/cafe/param/common"
 	mediaparam "github.com/miladshalikar/cafe/param/media"
 )
 
@@ -19,20 +19,44 @@ func (s Service) GetCategories(ctx context.Context, req categoryparam.GetCategor
 		return categoryparam.GetCategoryResponse{}, cErr
 	}
 
-	var categoriesInfo []categoryparam.CategoryInfo
+	var mediaIDs []uint
 	for _, category := range categories {
+		mediaIDs = append(mediaIDs, category.MediaID)
+	}
 
-		media, mErr := s.Client.GetURLMedia(ctx, mediaparam.GetURLRequest{ID: category.MediaID})
+	cachedURLs, err := s.cache.MGetMediaURLs(ctx, mediaIDs)
+	if err != nil {
+		return categoryparam.GetCategoryResponse{}, err
+	}
+
+	missingIDs := make([]uint, 0)
+	for _, id := range mediaIDs {
+		if _, found := cachedURLs[id]; !found {
+			missingIDs = append(missingIDs, id)
+		}
+	}
+
+	for _, id := range missingIDs {
+
+		mediaRes, mErr := s.Client.GetURLMedia(ctx, mediaparam.GetURLRequest{ID: id})
 		if mErr != nil {
 			return categoryparam.GetCategoryResponse{}, mErr
 		}
+
+		_ = s.cache.SetMediaURLByMediaID(ctx, id, mediaRes.URL)
+
+		cachedURLs[id] = mediaRes.URL
+
+	}
+
+	var categoriesInfo []categoryparam.CategoryInfo
+	for _, category := range categories {
 		categoriesInfo = append(categoriesInfo, categoryparam.CategoryInfo{
 			ID:      category.ID,
 			Title:   category.Title,
 			MediaID: category.MediaID,
-			URL:     media.URL,
+			URL:     cachedURLs[category.MediaID],
 		})
-
 	}
 
 	return categoryparam.GetCategoryResponse{
