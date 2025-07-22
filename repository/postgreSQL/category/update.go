0ece281993c7e41832cebee5895b924cc277fd0a
Milad Shalikar <miladsh199@gmail.com>
2025-07-22 16:24:59 +0330

    hotfix - categoryRepo for soft delete

--- repository/postgreSQL/category/update.go
+++ repository/postgreSQL/category/update.go
@@ -8,7 +8,7 @@ import (
 
 func (d *DB) UpdateCategory(ctx context.Context, category entity.Category) error {
 
-	query := `UPDATE categories SET title = $1, media_id = $2 WHERE id = $3`
+	query := `UPDATE categories SET title = $1, media_id = $2 WHERE id = $3 AND deleted_at IS NULL`
 
 	result, err := d.conn.ExecContext(ctx, query, category.Title, category.MediaID, category.ID)
 	if err != nil {
