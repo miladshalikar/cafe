4983b8c9fba9b9f61f5839488248ad8b929c0c98
Milad Shalikar <miladsh199@gmail.com>
2025-08-13 19:54:10 +0330

    hotfix - item param

--- param/item/info.go
+++ param/item/info.go
@@ -5,6 +5,7 @@ type ItemInfo struct {
 	Title       string  `json:"title"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
+	Quantity    uint    `json:"quantity"`
 	CategoryID  uint    `json:"category_id"`
 	MediaID     uint    `json:"media_id"`
 	URL         string  `json:"url"`
